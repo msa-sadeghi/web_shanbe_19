d91e9a31539539d5fff6ec0499e5c95b4ccf486a
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-08 19:07:19 +0330

    s

---
+++ javascript/app.js
@@ -0,0 +1,84 @@
+// for(let i =  0; i < 5; i++){
+//     console.log(`number# ${i}`)
+// }
+
+// let i = 0
+// while(i < 5){
+//     console.log(`item  # ${i}`)
+//     i++
+// }
+
+
+// let x = 0
+// x = x + 1
+// x += 1
+// x++
+
+// let something1 = "this is a test"
+// console.log(something1[0])
+// let something2 = 'this is a test'
+// console.log(something2[0])
+// let something3 = `this is a test`
+// console.log(something3[0])
+// console.log(something3.length)
+// let age = 12
+// if(age >= 18){
+//     console.log("age is greater than 18")
+// }else if (age < 13){
+//     console.log("age is less than 13")
+
+// }else if (age >= 13 && age <= 17){
+//     console.log("blalalal")
+// }
+
+// // &&     ||     !
+
+
+// let user = {
+//     username : "sara",
+//     password : "1234"
+// }
+// console.log(user.username)
+// console.log(user.password)
+// console.log(user['username'])
+// console.log(user['password'])
+
+
+// let userName = prompt("نام کاربری")
+// let x2 = Number(prompt("نام کاربری"))
+
+
+// let colors =  ["red",  "green", "blue"]
+// console.log(colors[0])
+// console.log("_________________")
+// for(let i = 0;  i < colors.length; i++){
+//     console.log(colors[i])
+// }
+
+// console.log("_________________")
+// colors.forEach(function(c){
+//     console.log(c)
+// })
+
+// let numbers = [1,2,3,4,5]
+// let sum = 0
+// numbers.forEach(function(x){
+//     sum += x
+// })
+// console.log(sum)
+
+// let x1 = "123"
+// console.log(typeof x1)
+
+// let x2 = Number("123")
+// console.log(typeof x2)
+
+// let x3 = x2.toString()
+// let x4 = String(x2)
+
+
+let number1 = 123
+let number2 = "123"
+
+console.log(number1 == number2)
+console.log(number1 === number2)
