d17a1047c55fb435f481649d7c41d4930bfcbd3e
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-05-02 19:04:53 +0330

    s

---
+++ react/my-shop/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import ProductCard from './components/ProductCard'
+function App(){
+    return (
+        <ProductCard inStock={true} productName="tablet"/>
+    )
+}
+export default App
